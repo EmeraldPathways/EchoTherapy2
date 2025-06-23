b85f64a61e815f1399b11631ecd451ebc6fc754a
EmeraldPathways <83232905+EmeraldPathways@users.noreply.github.com>
2025-06-23 18:39:43 +0100

    Update cookie-policy.tsx

--- frontend/src/pages/cookie-policy.tsx
+++ frontend/src/pages/cookie-policy.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Head from 'next/head';
 import Link from 'next/link';
-import NavBar from '@/components/NavBar';
+import dynamic from 'next/dynamic'; // ADD THIS LINE
+
+// Dynamically import NavBar with ssr: false
+const NavBar = dynamic(() => import('@/components/NavBar'), { ssr: false });
 
 const CookiePolicy: React.FC = () => {
   return (
